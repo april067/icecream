5760fc79f5de0ac261cf87bb352518f58c6c345e
april067 <143269646+april067@users.noreply.github.com>
2023-11-06 13:21:04 +0000

    Deploying to gh-pages from @ april067/icecream@b28a477ae6c16852f35ed138b5aee6412677a96a 🚀

---
+++ index.54a6cc70.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("menu-toggle"),s=(e.querySelectorAll(".line"),"open"),t="close",n=s;e.addEventListener("click",(function(){e.classList.contains(s)?(e.classList.remove(n),n=t):e.classList.contains(t)&&(e.classList.remove(n),n=s),e.offsetWidth,e.classList.add(n)}))}();
+//# sourceMappingURL=index.54a6cc70.js.map
---
+++ index.af9b0a3f.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("menu-toggle"),s=(e.querySelectorAll(".line"),"open"),t="close";let l=s;e.addEventListener("click",(()=>{e.classList.contains(s)?(e.classList.remove(l),l=t):e.classList.contains(t)&&(e.classList.remove(l),l=s),e.offsetWidth,e.classList.add(l)}));
+//# sourceMappingURL=index.af9b0a3f.js.map
